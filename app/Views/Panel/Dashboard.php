3f69a2c430ca4c51c59ac3e0f00b9d711cf2d874
mary-1802 <165608283+mary-1802@users.noreply.github.com>
2024-04-07 18:55:22 -0600

    Update Dashboard.php

--- app/Views/Panel/Dashboard.php
+++ app/Views/Panel/Dashboard.php
@@ -48,10 +48,10 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="index3.html" class="nav-link"> <i class="fa fa-id-badge" aria-hidden="true"></i> Perfil</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="<?= route_to('salir_administracion_acceso')?>" class="nav-link"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Salir</a>
       </li>
     </ul>
 
