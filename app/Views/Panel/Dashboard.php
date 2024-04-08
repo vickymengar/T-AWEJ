4ad79f035a9c3be39b666d262819b0192895be67
mary-1802 <165608283+mary-1802@users.noreply.github.com>
2024-04-07 18:10:51 -0600

    Update Dashboard.php

--- app/Views/Panel/Dashboard.php
+++ app/Views/Panel/Dashboard.php
@@ -191,10 +191,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url(RECURSOS_PANEL_DIST.'img/user2-160x160.jpg')?>" class="img-circle elevation-2" alt="User Image">
+          <img src="<?= base_url(RECURSOS_PANEL_IMG_PROFILES_USER.$imagen_usuario)?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?= $nombre_usuario ?></a>
         </div>
       </div>
 
