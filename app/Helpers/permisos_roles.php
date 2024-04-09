613bc5fc06dcc4597a003e150dd4d67a8efaf355
mary-1802 <165608283+mary-1802@users.noreply.github.com>
2024-04-09 11:10:34 -0600

    Update permisos_roles.php

--- app/Helpers/permisos_roles.php
+++ app/Helpers/permisos_roles.php
@@ -2,11 +2,15 @@
 function acceso_usuario($tarea_actual = "", $rol_actual = ""){
 $permiso = TRUE; 
  switch ($rol_actual) {
-case 'value':
-
+case ROL_ADMINISTRADOR["clave"]:
+  $permiso = in_array($tarea_actual, PERMISOS_ADMINISTRADOR);
 break;
-default: 
-
+  case ROL_OPERADOR["clave"]:
+  $permiso = in_array($tarea_actual, PERMISOS_ADMINISTRADOR);
+break;
+  
+default:
+  $permiso = FALSE;
 break;
 
  }
