3a62a8888ebfcabfc50c35d67d9808dc22959623
mary-1802 <165608283+mary-1802@users.noreply.github.com>
2024-04-09 13:41:08 -0600

    Update usuarios.php

--- app/Views/Panel/usuarios.php
+++ app/Views/Panel/usuarios.php
@@ -1,1 +1,15 @@
-//
+<?= $this->extend('plantillas/panel_base') ?>
+//seccion de css
+<?= $this->section('css') ?>
+<?= $this->endSection() ?>
+// end section
+
+//seccion de contenido
+<?= $this->section('contenido') ?>
+<?= $this->endSection() ?>
+//end seccion de contenido
+
+//seccion de js
+<?= $this->section('js') ?>
+<?= $this->endSection() ?>
+// end seccion js|
